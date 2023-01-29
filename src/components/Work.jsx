7c88a40d74c7728d3811d91866020798e54cb3fd
Udaceue <rjemik3@gmail.com>
2023-01-29 22:01:19 +0100

    bugfix:width changes in experience section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,7 +5,7 @@ function Work({data}) {
   return (
     <div id='work' className='min-h-max flex justify-center items-center flex-col p-4'>
     <h2 className='text-indigo-700 my-8 text-center text-4xl font-bold'>Experience</h2>
-    <div className='block  md:grid grid-cols-2 gap-4'>
+    <div className='block w-full md:grid grid-cols-2 gap-4'>
         {data.map((el, index)=>{
             return(
             <div key={index} className="p-2 border-b-blue-400 border-b-2">
